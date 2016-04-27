31661d024c16e88b59ffd594988cde76a04fa373
Nick Leone <nleone3@me.com>
2016-04-26 20:07:29 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160426213941) do
-
 
   create_table "pictures", force: :cascade do |t|
     t.string   "title"
