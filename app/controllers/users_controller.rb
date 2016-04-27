6fcc67ba9c447ee1217388392343702d19d7292b
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-04-27 11:32:34 -0400

    showed alex broken links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @user_pictures = @user.pictures
+    @user_pictures = (@user.pictures)
+  end
+
+  def index
+    @users = User.all
+  end
+
+
+  def avatar
+    @user_avatar = User.avatar
   end
 
 end
